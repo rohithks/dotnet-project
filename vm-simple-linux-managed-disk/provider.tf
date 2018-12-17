689fb9f98c95b83109dd8137478eda49615e04ea
rohithks <rohithkplm@gmail.com>
2018-12-17 09:13:34 +0000

    Create provider.tf

---
+++ vm-simple-linux-managed-disk/provider.tf
@@ -0,0 +1,12 @@
+# Microsoft Azure Resource Manager Provider
+
+
+# Uncomment this provider block if you are using variables (NOT environment variables)
+# to provide the azurerm provider requirements.
+#
+ provider "azurerm" {
+   subscription_id = "d4903dd0-8dc1-4289-a09a-5ba9d781828e"
+   client_id       = "6639cd0e-b4e1-40d7-b196-95808167ee35"
+   client_secret   = "WiEQKkR1nSBkGcH+w5PHZTxW9ZVEQjEMY8celZbkaVU="
+   tenant_id       = "3840e252-91d6-4a58-b388-05bd14ac3e24"
+ }
